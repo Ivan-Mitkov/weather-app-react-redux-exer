a37c5cad6a8952f2159a5a6cd14ebaf13a3f5fa1
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-02 08:17:40 +0300

    connecting to weather api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import APIKEY from './../api';
 
-const ROOT_URL=`https://samples.openweathermap.org/data/2.5/forecast?appid=${APIKEY}`
+const ROOT_URL=`https://api.openweathermap.org/data/2.5/forecast?`;
+const KEY=`appid=${APIKEY}`;
 export const FETCH_WEATHER='FETCH_WEATHER';
 
 export function fetchWeather(city){
-    const url=`${ROOT_URL}&q=${city},bg`;
+    const url=`${ROOT_URL}&q=${city},bg&${KEY}`;
     const request=axios.get(url);
     return{
         type:FETCH_WEATHER,
